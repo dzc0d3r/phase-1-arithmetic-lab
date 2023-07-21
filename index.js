956246a5247606085aeacc1308a548ae21d88cbd
dzc0d3r <m4dcod3r@protonmail.com>
2023-07-22 00:11:31 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+
+// mutiply
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+
+// Random
+const random = Math.floor(Math.random() * 10)
+
+
+// Mod
+const num3 = 25
+const num4 = 7
+const mod = num3 % num4
+
+// Max
+const max = Math.max(7, 5, 13, 8, 20, 15, 9)
